f538e1a0a8d82d8337e0bf09d4f588d6a272bced
epedrosa <epedrosa@gmail.com>
2020-06-10 17:43:47 -0300

    Versao inicial avaliacao Tokio marine

---
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -0,0 +1,79 @@
+package br.com.tokiomarine.seguradora.avaliacao.entidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Estudante {
+	// TODO Implementar a entidade Estudante conforme solicitado
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+    
+    @NotBlank
+    private String email;
+    
+    private Long telefone;
+    
+    @NotBlank
+    private Long matricula;
+    
+    @NotBlank
+    private String curso;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+
+	public Long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+}
---
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/service/EstudanteServiceImpl.java
@@ -0,0 +1,41 @@
+package br.com.tokiomarine.seguradora.avaliacao.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tokiomarine.seguradora.avaliacao.entidade.Estudante;
+import br.com.tokiomarine.seguradora.avaliacao.repository.EstudanteRepository;
+
+@Service
+public class EstudanteServiceImpl implements EstudandeService {
+
+	@Autowired
+	EstudanteRepository repository;
+
+	@Override
+	public void cadastrarEstudante(@Valid Estudante estudante) {
+		repository.save(estudante);
+
+	}
+
+	@Override
+	public void atualizarEstudante(@Valid Estudante estudante) {
+		repository.save(estudante);
+
+	}
+
+	@Override
+	public List<Estudante> buscarEstudantes() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Estudante buscarEstudante(long id) {
+		throw new IllegalArgumentException("Identificador inválido:" + id);
+	}
+
+}
